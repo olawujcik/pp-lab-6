f5bdc62dc6b06d77dff1bef09c4e0d1b29c46e46
Aleksandra Wojcik <olawujcik@wp.pl>
2024-05-17 19:38:46 +0200

    zad2

--- Main.java
+++ Main.java
@@ -1,21 +1,29 @@
 import company.models.Manager;
 import company.models.Worker;
+import company.abstracts.Employee;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        Worker worker1 = new Worker("Aleksandra Wójcik", 5300, 1);
-        Worker worker2 = new Worker("Adam Nowak", 4200, 2);
-        Worker worker3 = new Worker("Kamil Kowalski", 3200, 3);
-        Worker worker4 = new Worker("Oliwia Roza", 5100, 4);
-        Manager manager = new Manager("Kuba Wilk", 2700, 5);
+        Worker worker1 = new Worker("Aleksandra Wójcik", 5300, 1, "2024-05-17", "Worker");
+        Worker worker2 = new Worker("Adam Nowak", 4200, 2, "2022-11-01", "Worker");
+        Worker worker3 = new Worker("Kamil Kowalski", 3200, 3, "2019-04-01", "Worker");
+        Worker worker4 = new Worker("Oliwia Roza", 5100, 4, "2023-05-01", "Worker");
+        Manager manager = new Manager("Kuba Wilk", 2700, 5, "2003-01-01", "Manager");
 
-        Worker[] workers = {worker1, worker2, worker3, worker4};
-        for (Worker worker : workers) {
-            System.out.println("Wynagrodzenie " + worker.getName() + ": " + worker.getSalary());
-            worker.work();
+        List<Employee> employees = new ArrayList<>();
+        employees.add(worker1);
+        employees.add(worker2);
+        employees.add(worker3);
+        employees.add(worker4);
+        employees.add(manager);
+
+        for (Employee employee : employees) {
+            employee.work();
+            System.out.println("- " + employee.getName() + " (ID: " + employee.hashCode() +
+                    ", Position: " + employee.getPosition() + ", Hire date: " + employee.getHireDate() +
+                    ", Salary: " + employee.getSalary() + ")");
         }
-
-        System.out.println("Wynagrodzenie " + manager.getName() + ": " + manager.getSalary());
-        manager.work();
     }
 }
